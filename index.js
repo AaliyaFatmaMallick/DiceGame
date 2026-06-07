04f25d4ac7cff6dcab9b828162bfecd6fe316457
AaliyaFatmaMallick <86707171+AaliyaFatmaMallick@users.noreply.github.com>
2026-06-07 14:11:19 +0530

    Refactor dice game logic for clarity

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var randomNumber1=Math.floor((Math.random()*6)+1);
 var randomDiceImage="dice"+randomNumber1+".png";
-var randomImage="images/"+randomDiceImage;
+var randomImage=randomDiceImage;
 var Image1=document.querySelectorAll("img")[0].setAttribute("src",randomImage);
 
 var randomNumber2=Math.floor((Math.random()*6)+1);
 var randomDiceImage2="dice"+randomNumber2+".png"
-var randomImage2="images/"+randomDiceImage2;
+var randomImage2=randomDiceImage2;
 document.querySelectorAll("img")[1].setAttribute("src",randomImage2);
 
 if(randomNumber1>randomNumber2)
